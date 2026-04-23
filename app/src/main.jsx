bd9e308914c8f0a20264b5d80e2d7fedf96af59a
Achimari <achimari@gmail.com>
2026-04-23 23:36:59 +0300

    Link hotfix

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { I18nProvider } from "./i18n/index.jsx";
 import "./styles/globals.css";
 
+if (window.location.hash.startsWith("#/")) {
+  const legacyPath = window.location.hash.slice(1);
+  const basePath = window.location.pathname.replace(/\/$/, "");
+  const nextPath = `${basePath}${legacyPath}` || "/";
+  window.history.replaceState(null, "", `${nextPath}${window.location.search}`);
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <I18nProvider>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </I18nProvider>
   </React.StrictMode>
 );
